22c79145da36bb3bfa57e87b2ad0b890cb7214d5
j4sperneedshelp <147946914+j4sperneedshelp@users.noreply.github.com>
2026-04-29 11:39:53 -0700

    Update GuesserTest.cpp

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -20,3 +20,46 @@ TEST(GuesserTest, smoke_test)
   Guesser object("Secret");
   ASSERT_EQ( 1+1, 2 );
 }
+
+TEST(GuesserTest,three_tries) {
+	Guesser object("Secret");
+	object.match("wrong");
+	object.match("wrong");
+	object.match("wrong");
+	ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest,zero_length_guess) {
+	Guesser object("Secret");
+	ASSERT_FALSE(object.match(""));
+}
+
+TEST(GuesserTest,long_guess) {
+	Guesser object("Secret");
+	ASSERT_EQ(object.distance("Secret1"),1);
+}
+
+TEST(GuesserTest,short_guess) {
+	Guesser object("Secret");
+	ASSERT_EQ(object.distance("Secre"),1);
+}
+
+TEST(GuesserTest,same_len_guess) {
+	Guesser object("Secret");
+	ASSERT_EQ(object.distance("Secrel"),1);
+}
+
+TEST(GuesserTest,long_input) {
+	Guesser object("SecretSecretSecretSecretSecretSecret");
+	ASSERT_TRUE(object.match(SecretSecretSecretSecretSecretSe));
+}
+
+TEST(GuesserTest,correct_guess) {
+	Guesser object("Secret");
+	ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest,zero_distance) {
+	Guesser object("Secret");
+	ASSERT_EQ(object.distance("Secret"),0);
+}
